29d9355d263ffa4e44abc7fcfda6413ca1df0e4e
amineb55 <49524796+amineb55@users.noreply.github.com>
2026-06-18 12:30:16 +0100

    Fix android/app/src/main/java/com/rntestdemo/MainApplication.kt

--- android/app/src/main/java/com/rntestdemo/MainApplication.kt
+++ android/app/src/main/java/com/rntestdemo/MainApplication.kt
@@ -1,7 +1,6 @@
 package com.rntestdemo
 
 import android.app.Application
-import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
@@ -14,7 +13,7 @@ class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> = PackageList(this).packages
+        override fun getPackages(): List<ReactPackage> = emptyList()
         override fun getJSMainModuleName(): String = "index"
         override fun getUseDeveloperSupport(): Boolean = false
         override val isNewArchEnabled: Boolean = false
