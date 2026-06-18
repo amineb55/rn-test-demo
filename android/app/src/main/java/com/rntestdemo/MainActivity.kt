8175a0ca882d450c4d479f98087448e62b9f5e89
amineb55 <49524796+amineb55@users.noreply.github.com>
2026-06-18 12:50:07 +0100

    Upgrade android/app/src/main/java/com/rntestdemo/MainActivity.kt

--- android/app/src/main/java/com/rntestdemo/MainActivity.kt
+++ android/app/src/main/java/com/rntestdemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.rntestdemo
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -7,6 +8,11 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
   override fun getMainComponentName(): String = "rntestdemo"
+
+  // Requis par react-native-screens
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
