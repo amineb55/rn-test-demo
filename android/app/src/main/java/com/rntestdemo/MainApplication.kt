ec8cf2dedf675f2545503a2c3a0d5bde0a047d94
amineb55 <49524796+amineb55@users.noreply.github.com>
2026-06-18 10:41:43 +0100

    Add android/app/src/main/java/com/rntestdemo/MainApplication.kt

---
+++ android/app/src/main/java/com/rntestdemo/MainApplication.kt
@@ -0,0 +1,31 @@
+package com.rntestdemo
+
+import android.app.Application
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
+
+class MainApplication : Application(), ReactApplication {
+
+  override val reactNativeHost: ReactNativeHost =
+      object : DefaultReactNativeHost(this) {
+        override fun getPackages(): List<ReactPackage> = PackageList(this).packages
+        override fun getJSMainModuleName(): String = "index"
+        override fun getUseDeveloperSupport(): Boolean = false
+        override val isNewArchEnabled: Boolean = false
+        override val isHermesEnabled: Boolean = true
+      }
+
+  override val reactHost: ReactHost
+    get() = getDefaultReactHost(applicationContext, reactNativeHost)
+
+  override fun onCreate() {
+    super.onCreate()
+    load()
+  }
+}
